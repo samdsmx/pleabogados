14395886c4bebe42dc57b6a5f593b1e1760aa8e5
Héctor Niño <hector.nj47@gmail.com>
2020-04-26 22:38:12 -0500

    Se agregó formulario de contacto

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -643,36 +643,36 @@
 		}
 		
 		/* - Quick Contact Form* */
-		$( "#btn_submit" ).on( "click", function(event) {
-		  event.preventDefault();
-		  var mydata = $("form").serialize();
-			$.ajax({
-				type: "POST",
-				dataType: "json",
-				url: "contact.php",
-				data: mydata,
-				success: function(data) {
-					if( data["type"] == "error" ){
-						$("#alert-msg").html(data["msg"]);
-						$("#alert-msg").removeClass("alert-msg-success");
-						$("#alert-msg").addClass("alert-msg-failure");
-						$("#alert-msg").show();
-					} else {
-						$("#alert-msg").html(data["msg"]);
-						$("#alert-msg").addClass("alert-msg-success");
-						$("#alert-msg").removeClass("alert-msg-failure");					
-						$("#input_name").val("");
-						$("#input_phone").val("");												
-						$("#input_address").val("");										
-						$("#textarea_message").val("");
-						$("#alert-msg").show();				
-					}			
-				},
-				error: function(xhr, textStatus, errorThrown) {
-					alert(textStatus);
-				}
-			});
-		});
+		// $( "#btn_submit" ).on( "click", function(event) {
+		//   event.preventDefault();
+		//   var mydata = $("form").serialize();
+		// 	$.ajax({
+		// 		type: "POST",
+		// 		dataType: "json",
+		// 		url: "contact.php",
+		// 		data: mydata,
+		// 		success: function(data) {
+		// 			if( data["type"] == "error" ){
+		// 				$("#alert-msg").html(data["msg"]);
+		// 				$("#alert-msg").removeClass("alert-msg-success");
+		// 				$("#alert-msg").addClass("alert-msg-failure");
+		// 				$("#alert-msg").show();
+		// 			} else {
+		// 				$("#alert-msg").html(data["msg"]);
+		// 				$("#alert-msg").addClass("alert-msg-success");
+		// 				$("#alert-msg").removeClass("alert-msg-failure");					
+		// 				$("#input_name").val("");
+		// 				$("#input_phone").val("");												
+		// 				$("#input_address").val("");										
+		// 				$("#textarea_message").val("");
+		// 				$("#alert-msg").show();				
+		// 			}			
+		// 		},
+		// 		error: function(xhr, textStatus, errorThrown) {
+		// 			alert(textStatus);
+		// 		}
+		// 	});
+		// });
 		
 	});	/* - Document On Ready /- */
 	
